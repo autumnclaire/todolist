17689edc5d3ea2ab535bc841398823f33ec1f1f5
Autumn Victor <autumnclaire@yahoo.com>
2014-01-17 20:09:53 -0800

    Initial Commit

---
+++ ToDo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ToDo
+//
+//  Created by Autumn Victor on 1/17/14.
+//  Copyright (c) 2014 Autumn Victor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToDo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ToDo
+//
+//  Created by Autumn Victor on 1/17/14.
+//  Copyright (c) 2014 Autumn Victor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
